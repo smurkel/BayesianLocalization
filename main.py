92aa9449b4d3333ca75b97be4f89f6bdc35653fa
smurkel <mart_last@hotmail.com>
2022-02-08 09:40:07 +0100

    no comment

--- main.py
+++ main.py
@@ -6,24 +6,22 @@ from scipy.stats import lognorm, uniform, norm
 
 np.seterr(divide = 'ignore')
 
-img = Load("spot_x1_y2_s2_i2000_n8.tiff")
-
 """SET IMAGE AND MICROSCOPE PARAMETERS"""
 PSF_INTENSITY_MU = 1000
 PSF_INTENSITY_LOGNORM_S = 0.7
-PSF_RADIUS_MU = 1.7 # mean of the gaussian psf's standard deviation in units of pixels.
-PSF_RADIUS_LOGNORM_S = 0.4
+PSF_RADIUS_MU = 2.0 # mean of the gaussian psf's standard deviation in units of pixels.
+PSF_RADIUS_LOGNORM_S = 0.1
 
 NOISE_MEAN = 0
 NOISE_SIGMA = 2.0
 
-IMG_SIZE = 5
+IMG_SIZE = 4
 """DEFINE MCMC BEHAVIOUR"""
-MCMC_ITERATIONS = 1000
-MCMC_BURN = 500
-MCMC_XY_SIGMA = 0.5
-MCMC_RADIUS_SIGMA = 0.5
-MCMC_INTENSITY_SIGMA = 150.0
+MCMC_ITERATIONS = 100
+MCMC_BURN = 0
+MCMC_XY_SIGMA = 0.01
+MCMC_RADIUS_SIGMA = 0.01
+MCMC_INTENSITY_SIGMA = 5.0
 """DEFINE THE NOISE MODEL - IN PRACTICE, THE NOISE MODEL WILL BE DERIVED FROM IMAGES; IT IS A GAUSSIAN PDF WITH MEAN AND STD TAKEN FROM BACKGROUND PIXEL INTENSITY."""
 PDF_NOISE = norm(loc = NOISE_MEAN, scale = NOISE_SIGMA)
 """DEFINE PRIOR PROBABILITIES FOR X, Y, SIGMA, and INTENSITY."""
@@ -43,25 +41,27 @@ def prior_y(val):
 
 def prior_sigma(val):
     # sigma prior probability is: log normal distribution
-    if val > 0:
-        return PDF_SIGMA.pdf(val)
-    else:
-        return 0
+    return PDF_SIGMA.pdf(val)
 
 def prior_intensity(val):
     # intensity prior probability is lognormal.
-    if val > 0:
-        return PDF_INTENSITY.pdf(val)
-    else:
-        return 0
+    return PDF_INTENSITY.pdf(val)
 
 def mcmc_posterior_pdf(data):
-
+    """This can be improved by starting the random walk not at a random sample from the prior distributions, but instead near the mode of the likelihood. Also: the data should be cropped such that the mode of the likelihood
+    corresponds to the mean of the x and y prio dsitributions; i.e. crop with probable particle position in center.
+    Useful lecture: https://www.youtube.com/watch?v=pHsuIaPbNbY&ab_channel=MLSSIceland2014"""
     parameter_samples = list()
     x = (PDF_X.rvs(1), PDF_Y.rvs(1), PDF_SIGMA.rvs(1), PDF_INTENSITY.rvs(1))
+    x = (0.0, 0.0, 2.0, 1000.0)
     p_x = likelihood(data, x) + prior_probability(*x)
+
+    max_log = p_x
     for i in range(MCMC_ITERATIONS):
-        print("Iteration #{}".format(i))
+        if i % 10 == 0:
+            print("Iteration {}".format(i))
+        if p_x > max_log:
+            max_log = p_x
         # Step
         dx0 = np.random.normal(loc=0, scale=MCMC_XY_SIGMA)
         dx1 = np.random.normal(loc=0, scale=MCMC_XY_SIGMA)
@@ -72,19 +72,23 @@ def mcmc_posterior_pdf(data):
         y = (x[0]+dx0, x[1]+dx1, x[2]+dx2, x[3]+dx3)
 
         # Calculate new score
-        p_y = likelihood(data, y) + prior_probability(*y)
+        p_prior = prior_probability(*y)
+        p_data = likelihood(data, y)
+        p_y = p_data + p_prior
         R = p_y - p_x
+
         if R > 0:
-            parameter_samples.append(y)
+            parameter_samples.append([y[0], y[1], y[2], y[3], p_y])
             x = y
             p_x = p_y
         else:
-            if np.random.uniform(0, 1, 1) > np.exp(R):
-                parameter_samples.append(y)
-                x = y
-                p_x = p_y
+            if np.random.uniform(0, 1, 1) < np.exp(R):
+               parameter_samples.append([y[0], y[1], y[2], y[3], p_y])
+               x = y
+               p_x = p_y
             else:
-                parameter_samples.append(x)
+                parameter_samples.append([x[0], x[1], x[2], x[3], p_x])
+
     return parameter_samples[MCMC_BURN:]
 
 def prior_probability(x, y, sigma, intensity):
@@ -100,7 +104,7 @@ def likelihood(data, parameters):
     noise = data - model
     # 3 - evaluate the probability of observing this noise given the noise model
     log_probability = eval_noise(noise)
-    return False
+    return log_probability
 
 
 def eval_noise(noise):
@@ -111,29 +115,34 @@ def eval_noise(noise):
                 log_probability += np.log(PDF_NOISE.pdf(noise[i,j]))
     return log_probability
 
-data = simulator.particle(IMG_SIZE, 0, 0, 2, 5000.0) + simulator.noise(IMG_SIZE, 0, 0.2)
+ground_truth = (0.0, 0.0 ,2.0, 1000.0)
+data = simulator.particle(IMG_SIZE, *ground_truth) + simulator.noise(IMG_SIZE, 0, 1.0)
 
 prms = mcmc_posterior_pdf(data)
 prms = np.asarray(prms)
-
-
-n_bins = 30
-x = prms[:, 0] + IMG_SIZE
-y = prms[:, 1] + IMG_SIZE
+n_bins = 25
+x = prms[:, 0]
+y = prms[:, 1]
 s = prms[:, 2]
 i = prms[:, 3]
-plt.subplot(4,1,1)
+p = prms[:, 4]
+
+
+plt.subplot(5,1,1)
 plt.hist(x, bins = n_bins)
 plt.title("X")
-plt.subplot(4,1,2)
+plt.subplot(5,1,2)
 plt.hist(y, bins = n_bins)
 plt.title("Y")
-plt.subplot(4,1,3)
+plt.subplot(5,1,3)
 plt.hist(s, bins = n_bins)
 plt.title("S")
-plt.subplot(4,1,4)
+plt.subplot(5,1,4)
 plt.hist(i, bins = n_bins)
 plt.title("I")
+plt.subplot(5,1,5)
+plt.hist(p, bins = n_bins)
+plt.title("P")
 
 plt.figure(2)
 n = np.shape(x)[0]
@@ -142,9 +151,10 @@ c[:, 0] = np.linspace(0, 1, n)
 c[:, 1] = np.linspace(1, 0, n)
 c[:, 3] = np.zeros(n) + 0.5
 plt.imshow(data)
-plt.plot(x, y, linewidth = 1, color = (0.0, 0.0, 0.0))
+
+plt.plot(y + IMG_SIZE, x + IMG_SIZE, linewidth = 1, color = (0.0, 0.0, 0.0))
 for j in range(n):
-    plt.plot(x[j], y[j], markerfacecolor = c[j], marker = 'o', markeredgecolor = (1.0, 1.0, 1.0), markersize = 5)
+    plt.plot(y[j] + IMG_SIZE, x[j] + IMG_SIZE, markerfacecolor = c[j], marker = 'o', markeredgecolor = (1.0, 1.0, 1.0), markersize = 5)
 
 plt.figure(3)
 plt.subplot(1,3,1)
@@ -152,7 +162,7 @@ plt.imshow(data)
 plt.subplot(1,3,2)
 final_model = simulator.particle(IMG_SIZE, x[-1], y[-1], s[-1], i[-1])
 plt.imshow(final_model)
-plt.title("FInal model")
+plt.title("Final model")
 plt.subplot(1,3,3)
 plt.imshow(simulator.particle(IMG_SIZE,x[0],y[0],s[0],i[0]))
 plt.title("First non burn model")
@@ -166,4 +176,8 @@ plt.subplot(1,4,3)
 plt.plot(s)
 plt.subplot(1,4,4)
 plt.plot(i)
+
+plt.figure(5)
+plt.plot(p)
+plt.title("Estimated model accuracy vs. iteration nr.")
 plt.show()
